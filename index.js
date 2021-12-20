51f8656d5f160b9e9d23fecd97be7077f16b91b2
David <dmaster18@gmail.com>
2021-12-19 19:37:15 -0500

    Code modification

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-// Code your solution here! This is where I'll put my code
+function printString(myString) {
+	console.log(myString[0]);
 
+	if (myString.length > 1) {
+		let mySubString = myString.substring(1, myString.length);
+		printString(mySubString);
+	} else {
+		return true;
+	}
+}
 
